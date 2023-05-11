cecc8ab278237d9d7d15e8692525a618ec8f5247
Matt Hackmann <matt.hackmann@gmail.com>
2023-05-10 22:53:20 -0700

    reworked buttons into a class + individual debounce

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ led_pin = Pin("LED", Pin.OUT)
 # the pins to which the buttons are connected and the order
 # in which they should be assigned to the outgoing press
 button_pins = [6, 7, 8, 9, 10, 11, 12, 13]
-button_pin_map = {}
+buttons = []
 
 def connect_wlan():
     wlan = network.WLAN(network.STA_IF)
@@ -39,40 +39,45 @@ def connect_wlan():
 
 def send_button_press_event(button_id):
     # TODO: send to LifeLog endpoint when that's up and running
-    response = urequests.post(
-        'https://wx.hakkslab.io/observation',
-        headers={
-            'Content-Type': 'text/json',
-        },
-        data='{"stationKey":"LBTN", "stationName": "LifeButton", "observationType": "temperature", "observationValue": 42}'
-    )
-    print('Send press event')
-    response.close()
+    # response = urequests.post(
+    #     'https://wx.hakkslab.io/observation',
+    #     headers={
+    #         'Content-Type': 'text/json',
+    #     },
+    #     data='{"stationKey":"LBTN", "stationName": "LifeButton", "observationType": "temperature", "observationValue": 42}'
+    # )
+    print(f'Send press event for button {button_id}')
+    # response.close()
+
+class Button:
+    def __init__(self, pin_id, button_id):
+        self.pin = Pin(pin_id, Pin.IN, Pin.PULL_UP)
+        self.pin.irq(handler=handle_button_press, trigger=Pin.IRQ_RISING)
+        self.button_id = button_id
+        self.debounce_time = 0
+
+        print(f'Button {button_id} created on pin {pin_id}')
+
+    def handle_interrupt(self, pin):
+        if pin is self.pin and utime.ticks_ms() > self.debounce_time:
+            self.debounce_time = utime.ticks_ms() + 2000
+            send_button_press_event(self.button_id)
+            return True
+        return False
 
 def handle_button_press(pin):
-    pin.irq(handler=None)
-
     # loop through the buttons to find the one that raised the interrupt
-    for key in button_pin_map:
-        value = button_pin_map[key]
-        index, button = value
-        if pin is button:
-            print(f'Sending event for button #{index}')
-            send_button_press_event(index)
-            time.sleep(1)
+    for button in buttons:
+        if button.handle_interrupt(pin):
             break
-
-    pin.irq(handler=handle_button_press, trigger=Pin.IRQ_FALLING)
 
 
 def init_buttons():
+    global button_pins
+
     i = 0
     for pin_id in button_pins:
-        button = Pin(pin_id, Pin.IN, Pin.PULL_DOWN)
-        button.irq(handler=handle_button_press, trigger=Pin.IRQ_FALLING)
-        # save this as a tuple of the button assignment and the pin object
-        button_pin_map[pin_id] = (i, button)
-        print(f'Assigned pin {pin_id} to button #{i}')
+        buttons.append(Button(pin_id, i))
         i += 1
 
 def main():
